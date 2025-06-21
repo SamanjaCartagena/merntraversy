48cd7e3e0879cab4c9d3d9c535192b8af1c10011
Samanja Cartagena <c.samanja09@Pabs.local>
2025-06-21 15:20:10 -0400

    goals worked out

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,5 @@ const dotenv = require('dotenv').config()
 
 const port =process.env.PORT||5000
 const app = express()
-app.get('/api/goals',(req,res)=>{
-    res.send('Get Goals')
-})
+app.use('/api/goals', require('./routes/goalRoutes'))
 app.listen(port, ()=> console.log(`Server started on port ${port}`))
